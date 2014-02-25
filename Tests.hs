95aead4982865c8b33f6f8bc44f4f4bd170f6ab3
Alastair Lynn <arplynn@gmail.com>
2014-02-25 16:38:18 +0000

    Remove the integration/differention tests

--- Tests.hs
+++ Tests.hs
@@ -14,25 +14,6 @@ import Control.FRP.Wire
 import Control.Category
 import Control.Arrow
 import Control.Applicative
-import Control.Arrow.Operations(ArrowCircuit, delay)
-
-integrate :: (ArrowCircuit a) => a (b, b) b -> b -> a b b
-integrate add base = proc x -> do rec prevTotal <- delay base -< curTotal
-                                      curTotal <- add -< (prevTotal, x)
-                                  id -< curTotal
-
-differentiate :: (ArrowCircuit a) => a (b, b) b -> b -> a b b
-differentiate sub base = proc x -> do prevValue <- delay base -< x
-                                      sub -< (prevValue, x)
-
-numIntegrate :: (ArrowCircuit a, Num n) => n -> a n n
-numIntegrate dt = integrate (arr step) 0
-  where step (p, x) = p + dt*x
-
-numDifferentiate :: (ArrowCircuit a, Fractional n) => n -> a n n
-numDifferentiate dt = differentiate (arr step) 0
-  where step (prevValue, value) = (value - prevValue) * factor
-        factor = recip dt
 
 testWire :: (ArrowApply a, ArrowChoice a) => Wire a b c -> a [b] [c]
 testWire w = proc xs -> case xs of
@@ -46,10 +27,6 @@ main = hspec $ do
   describe "Wire" $ do
     it "respects identity" $ do
       testWire id [1, 2, 3] `shouldBe` [1, 2, 3]
-    it "can numerically integrate" $ do
-      testWire (numIntegrate 1) [1, 2, 3] `shouldBe` [1, 3, 6]
-    it "can numerically differentiate" $ do
-      testWire (numDifferentiate 1) [1, 2, 3] `shouldBe` [1, 1, 1]
     it "can implement arr" $ property $
       \f x -> let apped = apply f in
                 (testWire (arr apped) (x :: [Integer])) `shouldBe`
